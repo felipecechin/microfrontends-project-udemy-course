0eb1311d3d1c745e5f2a943dc0224cc59c40af16
Felipe Cechin Mello <ficechin@hotmail.com>
2023-06-26 22:45:24 -0300

    container deploy script

--- mfp/container/config/webpack.common.js
+++ mfp/container/config/webpack.common.js
@@ -1,3 +1,6 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
 module.exports = {
   module: {
     rules: [
@@ -15,5 +18,10 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
 }
