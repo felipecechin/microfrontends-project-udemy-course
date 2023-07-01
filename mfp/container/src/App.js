e4737c5818d27ce57e5f95242be7baba481f57d3
Felipe Cechin Mello <ficechin@hotmail.com>
2023-07-01 17:37:46 -0300

    resolving css conflicts

--- mfp/container/src/App.js
+++ mfp/container/src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
+import { createGenerateClassName, StylesProvider } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
-  return (<div>
-    <h1>Hi there</h1>
-    <hr />
-    <MarketingApp />
-  </div>)
+  return (
+    <BrowserRouter>
+      <StylesProvider
+        generateClassName={generateClassName}
+      >
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  )
 };
